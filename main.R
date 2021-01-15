441423f4d7bb7fdf9868f8ded3bdb508b11b1e28
Fabian Peters <fabian-peters@users.noreply.github.com>
2021-01-15 12:39:21 +0100

    First draft of rating over time plots

--- main.R
+++ main.R
@@ -4,13 +4,22 @@
 # Created on: 15.01.21
 
 library(dplyr)
+library(ggplot2)
 
 load("data/Apps.rda")
 
+# rating by app
 ratings_by_app <- Apps %>%
   group_by(App) %>%
   summarise(mean(Rating))
 
+# rating by app and version
 ratings_by_version <- Apps %>%
   group_by(App, App_Version) %>%
   summarise(mean(Rating))
+
+# rating over time by app
+p <- ggplot(Apps, aes(x = Review_date, y = Rating))
+p <- p + facet_wrap(App ~ .)
+p <- p + geom_line()
+p
